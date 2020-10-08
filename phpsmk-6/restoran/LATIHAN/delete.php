669d5ba95687c76ece70cd200788c3db8425d9ce
Suhailahnfsella <suhailahnfsella@gmail.com>
2020-10-09 00:44:17 +0700

    Tugas Minggu Ke-13
    Project PHP OOP Video PHP SMK 31-40

---
+++ phpsmk-6/restoran/LATIHAN/delete.php
@@ -0,0 +1,15 @@
+<?php 
+
+    require_once "../function.php";
+
+    // $id = 3;
+
+    $sql = "DELETE FROM tblkategori WHERE idkategori = $id";
+
+    $result = mysqli_query($koneksi, $sql);
+
+    echo $sql;
+    
+    header("location:http://localhost/phpsmk/restoran/kategori/select.php");
+
+?>
