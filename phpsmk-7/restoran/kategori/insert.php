ef12f3aaafd68d39db2a5628269c68154f9b3122
Suhailahnfsella <suhailahnfsella@gmail.com>
2020-10-14 22:55:39 +0700

    Tugas Minggu Ke-14
    Project PHP OOP Aplikasi Restoran (Video 41-45 PhpSmk)

---
+++ phpsmk-7/restoran/kategori/insert.php
@@ -0,0 +1,22 @@
+<h1>Insert Kategori</h1>
+<div class="form-group">
+    <form action="" method="POST">
+        <div class="form-group w-50">
+            <label for="">Nama Kategori</label>
+            <input type="text" name="kategori" required placeholder="isi kategori" class="form-control">
+        </div>
+        <div>
+            <input type="submit" name="simpan" value="simpan" class="btn btn-primary">
+        </div>
+    </form>
+</div>
+<?php
+if (isset($_POST['simpan'])) {
+    $kategori = $_POST['kategori'];
+
+    $sql = "INSERT INTO tblkategori VALUES ('','$kategori')";
+    $db->runSQL($sql);
+
+    header("location:?f=kategori&m=select");
+}
+?>
